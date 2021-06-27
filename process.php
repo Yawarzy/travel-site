6dec8e7089efedf6edb180088b798083d5d99702
Yawarzy <yawarbhatzy@gmail.com>
2021-06-27 11:38:39 +0530

    Test

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: https://yawarzy.github.io/');
+    header('Location: https://yawarzy.github.io/travello');
   }
